bc127d89545afe3e1b4c28d58e0110af2537b973
Yasser <y.alattas@gmail.com>
2025-12-12 06:18:38 +0300

    chore(singoz): include infra telemetry

---
+++ curl.sh
@@ -0,0 +1,4 @@
+curl -H 'host: localhost' http://localhost:8000/demo/error/zero
+curl -H 'host: localhost' http://localhost:8000/demo/error/key
+curl -H 'host: localhost' http://localhost:8000/demo/span/sample
+curl -H 'host: localhost' http://localhost:8000/demo/log/sample
